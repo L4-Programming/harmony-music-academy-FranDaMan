27820e5e193e8a1bb52c9b077e5bafe67bc68a6d
FranDaMan <1343045@hc.ac.je>
2024-11-07 11:09:13 +0000

    updated, removed test for javascrip compatability, and added the comment plan for logic

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-// javascript
-
-console.log("Page Reloaded");
+// Display a form to the user
+// Include input fields for the user's email address, level, and hours of study
+// Capture user's input on form submission
+// Get the user's email address-userEmail (string)
+// Get the user's level -UserLevel variables - conditional statement and buttons?
+// Get the user's hours of study - UserHours- Number
+// Validate the user's input-check if the us
+// Check if the user has selected a level-check level has been entered; check against allowed levels
+// Check if the user has provided an email address -  is it valid, does it have an @? has it been entered?
+// Check if the user has specified at least one hour of study - cannot be negative so use integers - must be equal to or greater than one.
+// Check if the number of hours requested is within the allowed range- less than one or equal to the maximum number for the level
+// Calculate the total cost
+// Calculate the total cost
+// Display the total cost to the user
